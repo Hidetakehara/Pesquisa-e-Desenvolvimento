b7515b28d597f23be89c149c529c732deab3b58b
Anderson Takehara <andersontakehara@gmail.com>
2026-05-18 17:15:35 -0300

    Curso Programação Java Modulo 2 18-05-2026

--- Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 2/perimeter/PerimeterRunner.java
+++ Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 2/perimeter/PerimeterRunner.java
@@ -1,6 +1,14 @@
 import edu.duke.*;
+import java.io.File;
 
 public class PerimeterRunner {
+    
+    public static void main (String[] args) {
+        PerimeterRunner pr = new PerimeterRunner();
+        pr.testPerimeter();
+        pr.testPerimeterMultipleFiles();
+    }
+    
     public double getPerimeter (Shape s) {
         // Start with totalPerim = 0
         double totalPerim = 0.0;
@@ -24,10 +32,77 @@ public class PerimeterRunner {
         Shape s = new Shape(fr);
         double length = getPerimeter(s);
         System.out.println("perimeter = " + length);
+        
+        int numPoints = getNumPoints(s);
+        System.out.println("number of points = "+ numPoints);
+        
+        double averageLength = getAverageLength(s);
+        System.out.println("average side length = " + averageLength);
+        
+        double largestSide = getLargestSide(s);
+        System.out.println("largest side = " + largestSide);
+        
+        double largestX = getLargestX(s);
+        System.out.println("largest X = " + largestX);
+    }
+   
+    public int getNumPoints(Shape s) {
+        int numPoints = 0;
+        for (Point p : s.getPoints()){
+            numPoints = numPoints + 1;
+        }
+        return numPoints;
+    }
+    
+    public double getAverageLength(Shape s) {
+        double totalPerim = getPerimeter(s);
+        int numSides = getNumPoints(s);
+        double average = totalPerim / numSides;
+        return average;
+    }
+    
+    public double getLargestSide(Shape s){
+        double largestSide = 0.0;
+        Point prevPt = s.getLastPoint();
+        for (Point currPt:  s.getPoints()){
+            double currDist = prevPt.distance(currPt);
+            if (currDist > largestSide){
+                largestSide = currDist;
+            }
+            prevPt = currPt;
+        }
+        return largestSide;
+    }
+    
+    public double getLargestX(Shape s){
+        double largestX = Double.MIN_VALUE;
+        for (Point p : s.getPoints()){
+            if (p.getX() > largestX) {
+                largestX = p.getX();
+            }
+            }
+        return largestX;
+        }
+        
+    public double getLargestPerimeterMultipleFiles() {
+        double largestPerim = 0.0;
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            Shape s = new Shape(fr);
+            double currPerim = getPerimeter(s);
+            if (currPerim > largestPerim) {
+                largestPerim = currPerim;
+            }
+        }
+        return largestPerim;
     }
 
-    public static void main (String[] args) {
-        PerimeterRunner pr = new PerimeterRunner();
-        pr.testPerimeter();
+    // Método que chama o cálculo e imprime o resultado
+    public void testPerimeterMultipleFiles() {
+        double largestPerim = getLargestPerimeterMultipleFiles();
+        System.out.println("Largest perimeter among selected files = " + largestPerim);
     }
+        
 }
+
