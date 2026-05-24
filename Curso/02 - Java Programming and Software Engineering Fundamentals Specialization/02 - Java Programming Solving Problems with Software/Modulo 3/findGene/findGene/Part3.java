474e2cae028efa96f7cc9d2bd65102abb4f2d823
Anderson Takehara <andersontakehara@gmail.com>
2026-05-24 10:55:11 -0300

    Curso java Modulo 3 24-05-2026

--- Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 3/findGene/findGene/Part3.java
+++ Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 3/findGene/findGene/Part3.java
@@ -15,4 +15,36 @@ public class Part3 {
         int secondIndex = stringb.indexOf(stringa, firstIndex + stringa.length());
         return secondIndex != -1;
     }
+    
+    public String lastPart(String stringa, String stringb) {
+        int index = stringb.indexOf(stringa);
+        if (index == -1) {
+            return stringb;
+        }
+        return stringb.substring(index + stringa.length());
+    }
+    
+    public void teste (){
+        // Casos de teste para twoOccurrences
+        String a1 = "by";
+        String b1 = "A story by Abby Long";
+        System.out.println("stringa: " + a1 + ", stringb: " + b1);
+        System.out.println("Resultado: " + twoOccurrences(a1, b1));
+        
+        String a2 = "a";
+        String b2 = "banana";
+        System.out.println("stringa: " + a2 + ", stringb: " + b2);
+        System.out.println("Resultado: " + twoOccurrences(a2, b2));
+        
+        String a3 = "atg";
+        String b3 = "ctgtatgta";
+        System.out.println("stringa: " + a3 + ", stringb: " + b3);
+        System.out.println("Resultado: " + twoOccurrences(a3, b3));
+        
+        // Testando lastPart
+        System.out.println("A parte do final após 'an' em 'banana' é: " + lastPart("an", "banana"));
+        System.out.println("A parte do final após 'zoo' em 'forest' é: " + lastPart("zoo", "forest"));
+    }
+    
+    
 }
