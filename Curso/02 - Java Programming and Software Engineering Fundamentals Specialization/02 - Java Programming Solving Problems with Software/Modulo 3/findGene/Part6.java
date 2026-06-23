15d71dfd11ea96b489e76699bcd936a570652dc7
Anderson Takehara <andersontakehara@gmail.com>
2026-06-23 18:17:22 -0300

    Curso java Modulo 3 23-06-2026

--- Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 3/findGene/Part6.java
+++ Curso/02 - Java Programming and Software Engineering Fundamentals Specialization/02 - Java Programming Solving Problems with Software/Modulo 3/findGene/Part6.java
@@ -17,24 +17,56 @@ public class Part6 {
                 currIndex = dnaStr.indexOf(stopCodon, currIndex + 1);
             }
         }
-    return dnaStr.length();
+    return -1;
     }
     
-    public String findGene(String dna){
-        int startIndex = dna.indexOf("ATG");
+    public String findGene(String dna, int where){
+        int startIndex = dna.indexOf("ATG", where);
         if (startIndex == -1){
             return "";
         }
         int taaIndex = findStopCodon(dna,startIndex,"TAA");
         int tagIndex = findStopCodon(dna,startIndex,"TAG");
         int tgaIndex = findStopCodon(dna,startIndex,"TGA");
-        int temp = Math.min(taaIndex,tagIndex);
-        int minIndex = Math.min(temp, tgaIndex);
-        if (minIndex == dna.length()){
+        //int temp = Math.min(taaIndex,tagIndex);
+        //int minIndex = Math.min(temp, tgaIndex);
+        int minIndex = 0;
+        if (taaIndex == -1 ||
+            (tgaIndex != -1 && tgaIndex < taaIndex)) {
+                minIndex = tgaIndex;
+            }
+        else {
+            minIndex = taaIndex;
+        }
+        
+        if (minIndex == -1 ||
+            (tagIndex != -1 && tagIndex < minIndex)) {
+                minIndex = tagIndex;
+            }
+        
+        if (minIndex == -1){
             return"";   
         }
         return dna.substring(startIndex,minIndex + 3);
+        
     }
+    
+    public void printAllGenes (String dna){
+        int startIndex = 0;
+        
+        while (true){
+            String currentGene = findGene(dna, startIndex);
+            
+            if (currentGene.isEmpty()){
+                break;
+            }
+            
+            System.out.println(currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex) + currentGene.length();
+                        
+        }
+    }
+    
     
     public void testFindStop(){
         
@@ -44,10 +76,21 @@ public class Part6 {
         dex = findStopCodon(dna,9,"TAA");
         if (dex != 21) System.out.println("error on 21");
         dex = findStopCodon(dna,1,"TAA");
-        if (dex != 26) System.out.println("error on 26");
+        if (dex != -1) System.out.println("error on 26");
         dex = findStopCodon(dna,0,"TAG");
-        if (dex != 26) System.out.println("error on 26 TAG");
+        if (dex != -1) System.out.println("error on 26 TAG");
         System.out.println("tests finished");      
         }
+        
+    public void testOn(String dna) {
+        System.out.println("Testing printAllGenes on " + dna);
+        printAllGenes (dna);
+    }
+    
+    public void test(){
+        testOn("ATGATCTAATTTATGCTGCAACGGTGAAGA");
+        testOn("");
+        testOn("ATGATCATAAGAAGATAATAGAGGGCCATGTAA");
+    }
 }
 
